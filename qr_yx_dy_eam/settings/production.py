693593ad6f80c36864421cb1fcfbc8f6caefa477
lin <ericlin.linye@gmail.com>
2024-08-08 15:23:48 +0800

    product init

--- qr_yx_dy_eam/settings/production.py
+++ qr_yx_dy_eam/settings/production.py
@@ -2,7 +2,54 @@ from .base import *
 
 DEBUG = False
 
+
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+ALLOWED_HOSTS = ["*"]
+
 try:
     from .local import *
 except ImportError:
-    pass
+    import secrets
+    
+    SECRET_KEY = secrets.token_urlsafe(32)
+
+__base_path__ = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+__log_path__ = os.path.join(__base_path__, "log")
+
+if not os.path.exists(__log_path__):
+    os.makedirs(__log_path__)
+from datetime import datetime
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "[{levelname}][{asctime}][{module}][{process:d}][{thread:d}][{message}]",
+            "style": "{",
+        },
+        "simple": {
+            "format": "{levelname} {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "file": {
+            'formatter': 'verbose',
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": os.path.join(__log_path__, 
+                                        f"django_logfile_{datetime.now().strftime('%Y_%m_%d_%H_%M_%S_%f')[:23]}.log"),
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "INFO",
+            "propagate": True,
+        },
+    },
+}
